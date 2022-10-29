ef760200fa4dafe77b099fa64f112d67b710b34d
Nick Buhrer <buehrern@student.ethz.ch>
2022-10-29 17:31:10 +0800

    main with errors to resolve

--- builders/__init__.py
+++ builders/__init__.py
@@ -1,2 +1,3 @@
 from builders.env_builder import build_env
 from builders.callback_builder import build_callback
+from builders.model_builder import build_model
--- main.py
+++ main.py
@@ -1,42 +1,37 @@
 from omegaconf import DictConfig, OmegaConf
-from builders import build_env, build_callback
+from builders import build_env, build_callback, build_model
 import hydra
 import os
 
 
 class Trainer:
-    def __init__(self, cfg):
+    def __init__(self, cfg: OmegaConf):
         self.cfg = cfg
-        self.train_env, self.eval_env, self.video_env = None, None, None
-        self.log_dir = None
-
         self._setup()
 
     def _setup(self):
         self._setup_log_dir()
         self.train_env, self.eval_env, self.video_env = build_env(self.cfg.env_name, self.log_dir, self.cfg.seed)
         self.cb = build_callback(self.cfg.env_name, self.log_dir, self.eval_env, self.cfg.eval, self.video_env)
+        self.model = build_model(self.cfg.env_name, self.train_env, self.cfg.model, self.log_dir, self.cfg.seed)
 
     def _setup_log_dir(self):
-        if self.cfg.safe_mult:
+        if self.cfg.model.safe_rl.safe_mult:
             self.log_dir = f"logs/safe_rl/{self.cfg.env_name}/"
         else:
             self.log_dir = f"logs/baseline/{self.cfg.env_name}/"
 
-    def _setup_callbacks(self):
-        pass
-
-    def _setup_model(self):
-        pass
-
     def train(self):
-        pass
+        self.model.learn(total_timesteps=self.cfg.train.time_steps, log_interval=5, callback=self.cb)
+        self.train_env.close()
+        self.eval_env.close()
+        self.video_env.close()
 
 
 @hydra.main(version_base=None, config_path="hydra_config", config_name="default")
 def main(cfg: DictConfig):
-    print(OmegaConf.to_yaml(cfg))
-
+    # print(OmegaConf.to_yaml(cfg))
+    trainer = Trainer(cfg)
 
 if __name__ == "__main__":
     print(os.getcwd())
