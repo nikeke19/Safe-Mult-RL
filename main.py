cbd6775c67a9853bdaa15a8f2eafe73e7bf475d6
Nick Buhrer <buehrern@student.ethz.ch>
2022-10-29 12:08:23 +0800

    extending hydra conf

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import os
 
 @hydra.main(version_base=None, config_path="hydra_config", config_name="default")
 def my_app(cfg: DictConfig):
-    # print(cfg)
     print(OmegaConf.to_yaml(cfg))
-    print(OmegaConf.create(cfg))
 
 
 if __name__ == "__main__":
