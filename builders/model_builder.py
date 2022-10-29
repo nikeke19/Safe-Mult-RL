cdc10289d1c6b2df8574595b6a80b279d341eac3
Nick Buhrer <buehrern@student.ethz.ch>
2022-10-29 17:23:18 +0800

    Updating model loader

--- builders/model_builder.py
+++ builders/model_builder.py
@@ -6,8 +6,12 @@ import os
 from models.common.util import get_linear_fn
 
 
-def _load_model(path):
-    return PPO
+def _load_model(path: str, model_type: str, training_env: Env) -> BaseAlgorithm:
+    """Loading the model"""
+    if model_type == "SAC":
+        return SAC.load(path, env=training_env)
+    else:
+        return PPO.load(path, env=training_env)
 
 
 def build_model(env_name: str, train_env: Env, cfg: DictConfig, log_dir: str, seed: int = 0) -> BaseAlgorithm:
@@ -22,7 +26,7 @@ def build_model(env_name: str, train_env: Env, cfg: DictConfig, log_dir: str, se
     """
     # Loading the model if it exists
     if cfg.load_model_dir is not None:
-        return _load_model(cfg.load_model_dir)
+        return _load_model(cfg.load_model_dir, cfg.name, train_env)
 
     # Setting up base kwargs
     cfg.base.tensorboard_log = os.path.join(log_dir, "tensorboard")
@@ -32,21 +36,21 @@ def build_model(env_name: str, train_env: Env, cfg: DictConfig, log_dir: str, se
     # Setting up env specific kwargs
     if env_name == "lunar_lander":
         policy = "MlpPolicy"
-        if cfg.model.name == "PPO":
+        if cfg.name == "PPO":
             cfg.base.ent_coef = get_linear_fn(0.02, 0, start_fraction=0.5, end_fraction=1)
-        elif cfg.model.name == "SAC":
+        elif cfg.name == "SAC":
             cfg.base.learning_rate = get_linear_fn(3e-4, 1e-8)
 
     else:
         raise NotImplemented(f"Env {env_name} is not implemented. Choose [lunar_lander, car_racing, point_navigation]")
 
     # Choosing correct model
-    if cfg.model.name == "PPO":
+    if cfg.name == "PPO":
         model_func = PPO
         policy = "Custom" + policy
-    elif cfg.model.name == "SAC":
+    elif cfg.name == "SAC":
         model_func = SAC
     else:
-        raise NotImplemented(f"Model {cfg.model.name} is not implemented. Choose from [SAC, PPO]")
+        raise NotImplemented(f"Model {cfg.name} is not implemented. Choose from [SAC, PPO]")
 
     return model_func(policy, train_env, seed=seed, **cfg.base, **cfg.safe_rl)
